d7a54b73040a4475485aae8855a79006720b7f81
Dmitry O <theicesilver121@mail.ru>
2024-03-05 16:46:06 +0300

    fixed

--- components/1-masthead/index.tsx
+++ components/1-masthead/index.tsx
@@ -24,7 +24,6 @@ const Masthead: React.FC = () => {
 		if (!pinContainerRef.current) {
 			return;
 		}
-		// const endValue = `+=${pinContainerRef.current.offsetHeight}`;
 		const endValue = `+=${pinContainerRef.current.offsetHeight * 2}`;
 
 		tl.current = gsap.timeline({
@@ -41,11 +40,11 @@ const Masthead: React.FC = () => {
 		});
 
 		tl.current
-			.fromTo(firstH1Ref.current, { y: -500 }, { duration: 1, autoAlpha: 1, y: 0 })
-			.to(backgroundImageRef.current, { duration: 1, scale: 1.4, ease: 'none' }, '<')
-			.fromTo(secondH1Ref.current, { y: -500 }, { duration: 1, autoAlpha: 1, y: 0 })
+			.fromTo(firstH1Ref.current, { y: -500 }, { duration: 1, autoAlpha: 1, y: 0, ease: 'none' })
+			.to(backgroundImageRef.current, { duration: 1, scale: 1.4, ease: 'po' }, '<')
+			.fromTo(secondH1Ref.current, { y: -500 }, { duration: 1, autoAlpha: 1, y: 0, ease: 'none' })
 			.to(backgroundImageRef.current, { duration: 1, scale: 1, ease: 'none' }, '<')
-			.fromTo(thirdH1Ref.current, { y: -500 }, { duration: 1, autoAlpha: 1, y: 0 })
+			.fromTo(thirdH1Ref.current, { y: -500 }, { duration: 1, autoAlpha: 1, y: 0, ease: 'none' })
 			.fromTo(arrowRef.current, { opacity: 0, y: 500 }, { duration: 1, autoAlpha: 1, y: 0 }, '<');
 
 		return () => {
@@ -57,36 +56,31 @@ const Masthead: React.FC = () => {
 			}
 			ScrollTrigger.getAll().forEach((instance) => instance.kill());
 		};
-
-		// return () => {
-		// 	if (tl.current) {
-		// 		tl.current.kill();
-		// 	}
-		// 	ScrollTrigger.getAll().forEach((instance) => instance.kill());
-		// };
 	}, []);
 
 	return (
-		<div className={`lol ${styles.lol}`} ref={pinContainerRef}>
-			<div className={styles.masthead} ref={mastheadRef}>
-				<div className={styles.masthead__container}>
-					<Title firstH1Ref={firstH1Ref} secondH1Ref={secondH1Ref} thirdH1Ref={thirdH1Ref} />
-					<div className={`${styles.arrowDown} hide-on-mobile`} ref={arrowRef}>
-						<ArrowDown />
-					</div>
+		<>
+			<div className={`lol ${styles.lol}`} ref={pinContainerRef}>
+				<div className={styles.masthead} ref={mastheadRef}>
+					<div className={styles.masthead__container}>
+						<Title firstH1Ref={firstH1Ref} secondH1Ref={secondH1Ref} thirdH1Ref={thirdH1Ref} />
+						<div className={`${styles.arrowDown} hide-on-mobile`} ref={arrowRef}>
+							<ArrowDown />
+						</div>
 
-					<Image
-						ref={backgroundImageRef}
-						className={styles.backgroundImage}
-						src='/1-masthead/bg.webp'
-						alt='bg-image'
-						width={2880 / 2}
-						height={1600 / 2}
-						priority={true}
-					/>
+						<Image
+							ref={backgroundImageRef}
+							className={styles.backgroundImage}
+							src='/1-masthead/bg.webp'
+							alt='bg-image'
+							width={2880 / 2}
+							height={1600 / 2}
+							priority={true}
+						/>
+					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
