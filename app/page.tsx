cfee125e9033708ecc3a72a558d0783ce1594d9f
Dmitry O <theicesilver121@mail.ru>
2024-03-05 10:52:43 +0300

    fixed: error path

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,4 @@
 import React from 'react';
-import Masthead from '@/components/masthead';
-import Mission from '@/components/mission';
-import Proprietary from '@/components/proprietary';
-import XRSuits from '@/components/xr-suits';
-import OurTeam from '@/components/our-team';
-import OurDivision from '@/components/our-division';
-import Partners from '@/components/partners';
-import Black from '@/components/black';
 import Container from '@/components/container';
 
 const Home = () => {
