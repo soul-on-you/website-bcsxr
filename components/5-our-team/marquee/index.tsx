56990966c766a20cd33ec8560bb5dc462b7cc856
Dmitry O <theicesilver121@mail.ru>
2024-03-06 18:13:45 +0300

    123fj

--- components/5-our-team/marquee/index.tsx
+++ components/5-our-team/marquee/index.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
 import styles from './styles.module.scss';
 import Marquee from 'react-fast-marquee';
 import Image from 'next/image';
+import useWindowSize from '@/hooks/useWindowSize';
 
 interface IMarqueeContainerProps {
 	direction: any;
 }
 
 const MarqueeContainer: React.FC<IMarqueeContainerProps> = ({ direction }) => {
+	const { width } = useWindowSize();
+	const isMobile = width < 768;
+
 	return (
 		<div className={styles.marqueeContainer}>
-			<Marquee direction={direction}>
+			<Marquee direction={direction} speed={isMobile ? 10 : 50}>
 				<div className={styles.imageWrapper}>
 					<Image src='/5-our-team/line.svg' alt='line' width={2800 / 2} height={500 / 2} />
 				</div>
